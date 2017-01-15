0a274bdb074db2e0e9a19add8f8f2957bf9202fd
Martendal <bouillon@et.esiea.fr>
2017-01-15 19:14:13 +0200

    Commit

--- WeatherBot.js
+++ WeatherBot.js
@@ -38,6 +38,27 @@ app.post('/webhook', (req, res) => {
 	}
 });
 
+app.post('/weather', (req, res) => {
+	if (req.body.result.action === 'weather') {
+		let city = req.body.result.parameters['geo-city'];
+		let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+'d0aad646908835ef3e99b559ff2d96c0'+'&q='+city;
+
+		request.get(restUrl, (err, response, body) => {
+			if (!err && response.statusCode == 200) {
+				let json = JSON.parse(body);
+				json.weather[0].description + ' and the temperature is ' + json.main.temp + ' ℉';
+				return res.json({
+					speech: msg,
+					displayText: msg,
+					source: 'weather'});
+			} else {
+				return res.status(400).json({
+					status: {
+						code: 400,
+						errorType: 'I failed to look up the city name.'}});
+			}})
+	}
+});
 
 function sendMessage(event) {
   let sender = event.sender.id;
