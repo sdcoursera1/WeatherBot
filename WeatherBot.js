52d7cc6c59dfc937d29f1c0b80154666f5637fa2
Martendal <bouillon@et.esiea.fr>
2017-01-15 18:47:53 +0200

    Commit

--- WeatherBot.js
+++ WeatherBot.js
@@ -1,47 +1,10 @@
-/*var apiai = require('apiai');
 
-var https = require('https');
-
-var options = {
-	hostname: 'api.api.ai',
-	port: 443,
-	path: '/v1/query',
-	method: 'GET',
-	header:{
-		'Authorization':'Bearer 8f013d59656846fca2f064ad5f127f2c'
-	}
-};
-
-var app = apiai("8f013d59656846fca2f064ad5f127f2c", options);
-
-
-var reqOptions = {
-	lang: "fr",
-	timezone: "Europte/Paris",
-	sessionId: "SessionID"
-}
-var request = app.textRequest*/
-
-/*var callback=function(data) {
-	console.log(data);
-}
-
-https.get(options, function(res) {
-	var body='';
-	res.on('data', function(data){
-		body = body + data;
-	});
-	res.on('end', function(){
-		var result=JSON.parse(body);
-		callback(result);
-	});
-}).on('error', function(e){
-	console.log('Error: '+e);
-});*/
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+//const app = express();
+const apiai = require('apiai');
+const app = apiai('8f013d59656846fca2f064ad5f127f2c');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -75,25 +38,22 @@ app.post('/webhook', (req, res) => {
 	}
 });
 
-/*
+
 function sendMessage(event) {
   let sender = event.sender.id;
   let text = event.message.text;
 
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: EAAY119ZCG2X4BAHeKFpQRCtxtKmxCBjHbR1w3czdWK6NjQzekqJZBhlj4C1LeCHA2Yk4D9ZAi3eMyeuldsaCE57hZBZBIOf9bojJBBJsOvVjVVB8EP7LvP5Tux3u9p5kOQ9BrehZCIreLpPK3tZB11RKgg3XTZCA6RRLS7QnQ8IicQZDZD},
-    method: 'POST',
-    json: {
-      recipient: {id: sender},
-      message: {text: text}
-    }
-  }, function (error, response) {
-    if (error) {
-        console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-        console.log('Error: ', response.body.error);
-    }
+  let apiai = apiaiApp.textRequest(text, {
+    sessionId: 'tabby_cat' // use any arbitrary id
   });
+
+  apiai.on('response', (response) => {
+    // Got a response from api.ai. Let's POST to Facebook Messenger
+  });
+
+  apiai.on('error', (error) => {
+    console.log(error);
+  });
+
+  apiai.end();
 }
-*/
